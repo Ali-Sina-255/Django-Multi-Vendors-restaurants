6b2679c2447e7ad8bec2ea5d6d2576a824ae3bc3
dev <dev@DEV.DEV>
2024-05-19 11:05:35 +0430

    vendor details page is added

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from . import views
 
-
-
 urlpatterns = [
     path('', views.marketplace_view,name='marketplace'),
     path('<slug:vendor_slug>/', views.market_palce_detail, name='vendor_detail')
