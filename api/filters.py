2c3dc9516d9d81d80be6a305f50892bb53418e54
Ali Sina Sultani <alisinasultani255@gmail.com>
2024-07-19 15:10:06 +0430

    vendor searching and sorting and filtering

--- api/filters.py
+++ api/filters.py
@@ -1,10 +1,42 @@
 from django_filters import FilterSet
+from accounts.models import User
 from menu.models import Category , FootItem
+from vendor.models import Vendor
 
 
 class CategoryFilter(FilterSet):
     class Meta:
         model = Category
         fields = {
-            'category_name':['exact']
+            'category_name':['exact'],
+            'vendor': ['exact']
         }
+    
+class UserFilterClass(FilterSet):
+    class Meta:
+        model = User
+        fields = {
+            "role": ['exact']
+        
+        }
+        
+class FoodItemFilterClass(FilterSet):
+    class Meta:
+        model = FootItem
+        fields = {
+            "food_title": ['exact'],
+            'category':['exact'],
+            'vendor': ['exact'],
+            'price':['lt', 'gt']
+        }
+        
+        
+class VendorFilterClass(FilterSet):
+    class Meta:
+        model = Vendor
+        fields =  {
+            'user':['exact'],
+            "vendor_name": ['exact'],
+            "is_approved" : ['exact'],
+            "update_om": ['exact']
+            }
--- api/views.py
+++ api/views.py
@@ -4,23 +4,31 @@ from rest_framework import status
 from menu.models import Category, FootItem
 from vendor.models import Vendor
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
+from .filters import CategoryFilter, UserFilterClass, FoodItemFilterClass, VendorFilterClass
+from .pagination import DefaultPaginationClass
 
-from .filters import CategoryFilter
 from rest_framework.viewsets import ModelViewSet
 
 
 class CategoryApiViewSet(ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_class = CategoryFilter
-    
+    pagination_class = DefaultPaginationClass
+    search_fields = ['category_name', 'description']
+    ordering_fields = ["category_name", "vendor","updated_at"]
     
     
     
 class FoodItemApiViewSet(ModelViewSet):
     queryset = FootItem.objects.all()
+    pagination_class = DefaultPaginationClass
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_class = FoodItemFilterClass
     
+    ordering_fields = ["price", "is_available","updated_at"]
     def get_serializer_class(self):
         return FoodItemSerializer
     
@@ -28,6 +36,11 @@ class FoodItemApiViewSet(ModelViewSet):
 class UserApiViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    filter_backends = [DjangoFilterBackend,SearchFilter,OrderingFilter]
+    filterset_class    = UserFilterClass
+    pagination_class = DefaultPaginationClass
+    ordering_fields = ['role', 'updated_at']
+    search_fields = ['role', 'first_name','last_name','phone_number']
 
 class UserProfileAPiViewSet(ModelViewSet):
     queryset = UserProfile.objects.all()     
@@ -36,5 +49,8 @@ class UserProfileAPiViewSet(ModelViewSet):
 class VendorApiViewSet(ModelViewSet):
     queryset = Vendor.objects.all()
     serializer_class = VendorSerializer
-    
+    filter_backends = [DjangoFilterBackend,SearchFilter,OrderingFilter]
+    filterset_class = VendorFilterClass
+    pagination_class = DefaultPaginationClass
+
     
